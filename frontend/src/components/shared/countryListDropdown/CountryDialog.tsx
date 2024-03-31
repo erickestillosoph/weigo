77f5b4d88d16e150bfdf7c28e511a9c330f31fc0
Erick Estilloso <hover.erickestilloso@gmail.com>
2024-03-31 23:44:58 +0800

    [develop] - added favicon and change the overlay of the dialog

--- frontend/src/components/shared/countryListDropdown/CountryDialog.tsx
+++ frontend/src/components/shared/countryListDropdown/CountryDialog.tsx
@@ -25,7 +25,7 @@ function CountryDialog() {
                     ></ChevronDownIcon>
                 </Dialog.Trigger>
                 <Dialog.Portal>
-                    <Dialog.Overlay className="z-20 bg-white/50 data-[state=open]:animate-overlayShow fixed inset-0" />
+                    <Dialog.Overlay className="z-20 bg-black/50 data-[state=open]:animate-overlayShow fixed inset-0" />
 
                     <Dialog.Content className="z-20 max-h-[70vh] w-[90%] min-w-[150px] fixed inset-0 rounded-[16px] m-auto bg-gray-100 p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
                         <Dialog.Title className="w_heading_primary font_inter m-0 text-[20px] font-medium sm:mt-0 mt-8">
