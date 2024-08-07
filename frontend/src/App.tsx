2039169b6181571b419729dc87d043e7d291663f
Erick Estilloso <revestilloso@gmail.com>
2024-08-08 05:05:11 +0800

    feat - added and setup local storage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,22 @@
 import "./App.css";
 import { RecoilRoot } from "recoil";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Header } from "./components/layouts/headerLayout";
 import { FooterLayout } from "./components/layouts/footerLayout";
-
 import { BodyLayout } from "./components/layouts/bodyLayout";
+
+const queryClient = new QueryClient();
+
 function App() {
     return (
         <div style={{ position: "relative" }}>
-            <RecoilRoot>
-                <Header></Header>
-                <BodyLayout></BodyLayout>
-                <FooterLayout className="margin_top_200_clamp"></FooterLayout>
-            </RecoilRoot>
+            <QueryClientProvider client={queryClient}>
+                <RecoilRoot>
+                    <Header></Header>
+                    <BodyLayout></BodyLayout>
+                    <FooterLayout className="margin_top_200_clamp"></FooterLayout>
+                </RecoilRoot>
+            </QueryClientProvider>
         </div>
     );
 }
