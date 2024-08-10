f64355dd4be86cbf5659ddcb48f29f1a70789927
Erick Estilloso <revestilloso@gmail.com>
2024-08-11 03:16:16 +0800

    types for dragonpay and guest accounts

--- frontend/src/datasources/dragonpay/dragonpayTypes.tsx
+++ frontend/src/datasources/dragonpay/dragonpayTypes.tsx
@@ -1,25 +1,25 @@
 export type Payment = {
     message: string;
     status: string;
-    code: string;
+    code: number;
 };
 export type CreditCard = {
     message: string;
     status: string;
-    code: string;
+    code: number;
 };
 export type ServiceModel = {
     message: string;
     status: string;
-    code: string;
+    code: number;
 };
 export type FilteredPayments = {
     message: string;
     status: string;
-    code: string;
+    code: number;
 };
 export type PreSelectingPayments = {
     message: string;
     status: string;
-    code: string;
+    code: number;
 };
